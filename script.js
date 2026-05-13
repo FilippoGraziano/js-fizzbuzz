8f6677e69637d07d3122c01f8a091274dea26c09
Filippo Graziano <filippograziano13@gmail.com>
2026-05-13 15:47:47 +0200

    add comment

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+//r Mostra tutti i numeri da 0 a 99
 for (i = 0; i < 100; i++) {
     let value = i + 1;
+
+    //r Se il numero è divisibile sia per 3 che per 5 allora tranformalo in "FizzBuzz"
     if (value % 3 === 0 && value % 5 === 0) value = `FizzBuzz`;
+    //r Se il numero è divisibile per 3 allora diventa "Fizz"
     else if (value % 3 === 0) value = `Fizz`;
+    //r Se il numero è divisibile per 5 allora diventa "Buzz"    
     else if (value % 5 === 0) value = `Buzz`;
+
+    //r Stampa tutti i valori in console
     console.log (value);
 }
