ab3faa0ca61e9350deb5cdaf264861796dd49ab8
Filippo Graziano <filippograziano13@gmail.com>
2026-05-13 15:42:34 +0200

    transform number in Fizz, Buzz and FizzBuzz

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 for (i = 0; i < 100; i++) {
-    console.log (i + 1)
+    let value = i + 1;
+    if (value % 3 === 0 && value % 5 === 0) value = `FizzBuzz`;
+    else if (value % 3 === 0) value = `Fizz`;
+    else if (value % 5 === 0) value = `Buzz`;
+    console.log (value);
 }
